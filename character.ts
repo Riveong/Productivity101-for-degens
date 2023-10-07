b325b028cf3c9661f631650a7c5e138fdb5f3bc5
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-10-07 15:49:37 +0000

    episode 1

---
+++ character.ts
@@ -0,0 +1,10 @@
+var bennet = "bouken"
+bennet = "best support ever"
+
+let raiden = "big bobba sword"
+raiden = "useless sad emo stupid archon"
+
+const hutao = "useless"
+hutao = "best dps in the game"
+
+console.log(hutao)
